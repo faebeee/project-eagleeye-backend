864b94ffdd62b7ee60eb649f7a767e53efc037f9
Fabio Gianini <faebeee@gmail.com>
2023-09-10 21:50:06 +0200

    Log token

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,6 +2,7 @@ import fp from 'fastify-plugin';
 import bearerAuthPlugin from '@fastify/bearer-auth';
 
 export default fp(async function (fastify, opts) {
+  console.log(process.env.AUTH_SECRET);
   fastify.register(bearerAuthPlugin, {
     keys: new Set([process.env.AUTH_SECRET])
   })
