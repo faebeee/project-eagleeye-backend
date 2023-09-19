d54f8d7f40f1ae6021e11a43baf242045a4c3048
Fabio Gianini <faebeee@gmail.com>
2023-09-19 13:38:57 +0200

    Add caching to load

--- routes/load/index.js
+++ routes/load/index.js
@@ -1,7 +1,26 @@
 import { getLoadStats } from '../../lib/load/loadtest.js';
+import NodeCache from 'node-cache';
 
 export default async function(fastify, opts) {
-  fastify.get('/', async function(request, reply) {
-    return getLoadStats(request.query.site);
+  const cache = new NodeCache();
+
+  fastify.get('/', async function(req, reply) {
+    const url = req.query.site;
+    if (cache.has(url)) {
+      console.log('route /load cache hit');
+      return cache.get(url);
+    }
+    cache.set(url, { status: 'loading', result: null });
+    getLoadStats(url)
+      .then((result) => {
+        console.log('route /load cache populated');
+        cache.set(url, { status: 'loaded', result });
+      })
+      .catch((e) => {
+        console.log('route /load failed', e.message);
+        cache.del(url);
+      });
+
+    return cache.get(url);
   });
 };
--- routes/pagespeed/index.js
+++ routes/pagespeed/index.js
@@ -13,13 +13,19 @@ export default async function(fastify, opts) {
   fastify.get('/', async (req, res) => {
     const url = req.query.site;
     if (cache.has(url)) {
+      console.log('route /pagespeed cache hit');
       return cache.get(url);
     }
 
     cache.set(url, { status: 'loading', result: null });
     getPageSpeed(url)
       .then((result) => {
+        console.log('route /pagespeed cache populated');
         cache.set(url, { status: 'loaded', result });
+      })
+      .catch((e) => {
+        console.log('route /pagespeed failed', e.message);
+        cache.del(url);
       });
 
     return cache.get(url);
